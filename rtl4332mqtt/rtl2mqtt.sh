9820c50c5c2a3a3f768b6ead3dbaf918b357464d
Tom Olesen <tomolesen@Toms-MacBook-Pro-645.local>
2021-01-01 21:43:14 +0100

    Locked rtl_433 to version 20.02

--- rtl4332mqtt/rtl2mqtt.sh
+++ rtl4332mqtt/rtl2mqtt.sh
@@ -164,10 +164,10 @@ MQTT_HOST="$(jq --raw-output '.mqtt_host' $CONFIG_PATH)"
 MQTT_USER="$(jq --raw-output '.mqtt_user' $CONFIG_PATH)"
 MQTT_PASS="$(jq --raw-output '.mqtt_password' $CONFIG_PATH)"
 MQTT_TOPIC="$(jq --raw-output '.mqtt_topic' $CONFIG_PATH)"
-PROTOCOL="$(jq --raw-output '.protocol' $CONFIG_PATH)"
+#PROTOCOL="$(jq --raw-output '.protocol' $CONFIG_PATH)"
 FREQUENCY="$(jq --raw-output '.frequency' $CONFIG_PATH)"
-GAIN="$(jq --raw-output '.gain' $CONFIG_PATH)"
-OFFSET="$(jq --raw-output '.frequency_offset' $CONFIG_PATH)"
+#GAIN="$(jq --raw-output '.gain' $CONFIG_PATH)"
+#OFFSET="$(jq --raw-output '.frequency_offset' $CONFIG_PATH)"
 
 # Start the listener and enter an endless loop
 echo "Starting RTL_433 with parameters:"
@@ -175,14 +175,15 @@ echo "MQTT Host =" $MQTT_HOST
 echo "MQTT User =" $MQTT_USER
 echo "MQTT Password =" $MQTT_PASS
 echo "MQTT Topic =" $MQTT_TOPIC
-echo "RTL_433 Protocol =" $PROTOCOL
+#echo "RTL_433 Protocol =" $PROTOCOL
 echo "RTL_433 Frequency =" $FREQUENCY
-echo "RTL_433 Gain =" $GAIN
-echo "RTL_433 Frequency Offset =" $OFFSET
+#echo "RTL_433 Gain =" $GAIN
+#echo "RTL_433 Frequency Offset =" $OFFSET
 
 #set -x  ## uncomment for MQTT logging...
 
-/usr/local/bin/rtl_433 -F json -R $PROTOCOL -f $FREQUENCY -g $GAIN -p $OFFSET | while read line
+#/usr/local/bin/rtl_433 -F json -R $PROTOCOL -f $FREQUENCY -g $GAIN -p $OFFSET | while read line
+/usr/local/bin/rtl_433 -F json -f $FREQUENCY | while read line
 do
   DEVICE="$(echo $line | jq --raw-output '.model' | tr -s ' ' '_')" # replace ' ' with '_'
   DEVICEID="$(echo $line | jq --raw-output '.id' | tr -s ' ' '_')"
